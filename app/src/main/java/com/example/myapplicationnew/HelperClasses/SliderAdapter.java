1a278509cffc728a5fe15ca76df31340f5fb1d7b
Aniket <waniket48@gmail.com>
2022-06-09 19:04:00 +0530

    commit Splash Screen + onboarding screen +dimple dashboard

---
+++ app/src/main/java/com/example/myapplicationnew/HelperClasses/SliderAdapter.java
@@ -0,0 +1,81 @@
+package com.example.myapplicationnew.HelperClasses;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.myapplicationnew.R;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    int[] images = {
+            R.drawable.onboarding1,
+            R.drawable.onboarding2,
+            R.drawable.onboarding3,
+            R.drawable.onboarding4
+    };
+
+    int[] headings = {
+            R.string.title1,
+            R.string.title2,
+            R.string.title3,
+            R.string.title4
+    };
+
+    int[] descriptions = {
+            R.string.slider1decs,
+            R.string.slider2decs,
+            R.string.slider3decs,
+            R.string.slider4decs
+    };
+
+
+    @Override
+    public int getCount() {
+        return headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (ConstraintLayout)object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slides_layout,container,false);
+
+        //Hooks
+        ImageView imageView = view.findViewById(R.id.slider_image);
+        TextView heading = view.findViewById(R.id.slider_heading);
+        TextView desc = view.findViewById(R.id.slider_disc);
+
+        imageView.setImageResource(images[position]);
+        heading.setText(headings[position]);
+        desc.setText(descriptions[position]);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout)object);
+    }
+}
---
+++ app/src/main/java/com/example/myapplicationnew/User/UserDashboard.java
@@ -0,0 +1,18 @@
+package com.example.myapplicationnew.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.myapplicationnew.R;
+
+public class UserDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+    }
+}
